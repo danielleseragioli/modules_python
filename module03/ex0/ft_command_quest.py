ca3fa3daef522fc9727575f1ab1b10c1f10e2d26
Your Name <danielle.seragioli@gmail.com>
2026-02-13 21:17:56 +0000

    add mod03

---
+++ module03/ex0/ft_command_quest.py
@@ -0,0 +1,17 @@
+import sys
+
+def command_quest():
+    args = sys.argv
+    print("=== Command Quest ===")
+    if len(args) < 2:
+        print("No arguments provided!")
+    program_name = args[0].replace('_', r'\_')
+    print(f"Program name: {program_name}")
+    if len(args) > 1:
+        print(f"Arguments received: {len(args) - 1}")
+    for i, arg in enumerate(args[1:], start=1):
+        print(f"Argument {i}: {arg}")
+    print(f"Total arguments: {len(args)}")
+
+if __name__ == "__main__":
+    command_quest()
